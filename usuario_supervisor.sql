167c9a87657abdb843bab3d66ae6a7aa436d7a7e
valnava147-prog <valnava147@gmail.com>
2026-05-18 13:21:04 -0600

    ahora si jefe

--- usuario_supervisor.sql
+++ usuario_supervisor.sql
@@ -1,2 +1,16 @@
+CREATE TABLE usuario (
+CURP VARCHAR (18) PRIMARY KEY NOT NULL
+);
+
+INSERT INTO usuario (CURP) VALUES (' '); 
 SELECT * FROM usuario; 
- SELECT * FROM supervisor; 
+
+CREATE TABLE supervisor ( 
+id_supervisor INT AUTO_INCREMENT PRIMARY KEY, 
+num_empleado VARCHAR (20) UNIQUE NOT NULL, 
+contraseña VARCHAR (20) UNIQUE NOT NULL,
+rol ENUM ("Trabajador" , "supervisor") NOT NULL
+);
+
+INSERT INTO supervisor (num_empleado, contraseña, rol) VALUES ('');
+ SELECT * FROM supervisor;
